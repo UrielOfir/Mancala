e3d2ba5c1efdb679f55b44b0521ab075d338d98d
Isaac Hagoel <sitzikh@gmail.com>
2020-12-04 17:06:05 +1100

    fixed webpack, removed dist from repo

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,41 @@
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
- "mode": "none",
- "entry": "./src/mancala.js",
- "output": {
-   "path": __dirname + '/dist',
-   "filename": "bundle.js"
- },
-devServer: {
-   contentBase: path.join(__dirname, 'dist')
- },
- "module": {
-  "rules": [
-    {
-      "test": /\.css$/,
-      "use": [
-        "style-loader",
-        "css-loader"
-      ]
+     "mode": "development",
+     "entry": "./src/mancala.js",
+     "output": {
+       "path": __dirname + '/dist',
+       "filename": "bundle.js"
+     },
+    devServer: {
+        contentBase: __dirname + '/dist',
     },
-    {
-      "test": /\.js$/,
-      "exclude": /node_modules/,
-      "use": {
-        "loader": "babel-loader",
-        "options": {
-          "presets": [
-            "@babel/preset-env",
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        })
+    ],
+     "module": {
+      "rules": [
+        {
+          "test": /\.css$/,
+          "use": [
+            "style-loader",
+            "css-loader"
           ]
-        }
-      }
-    },
-  ]
-},
+        },
+        {
+          "test": /\.js$/,
+          "exclude": /node_modules/,
+          "use": {
+            "loader": "babel-loader",
+            "options": {
+              "presets": [
+                "@babel/preset-env",
+              ]
+            }
+          }
+        },
+      ]
+    }
 }
